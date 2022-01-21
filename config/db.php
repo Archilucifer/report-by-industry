45599b99e109b4786553b09ea3aafcd5f5216a51
Nikita Hohlov <nikita.hohlov@cauri.com>
2022-01-21 16:57:37 +0300

    Поправил конфигурацию докера

--- config/db.php
+++ config/db.php
@@ -1,14 +1,11 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+use yii\db\Connection;
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+return [
+    'class' => Connection::class,
+    'dsn' => 'mysql:host=localhost;dbname=reports',
+    'username' => 'root',
+    'password' => 'example',
+    'charset' => 'utf8',
 ];
